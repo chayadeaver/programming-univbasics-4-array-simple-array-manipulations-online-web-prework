823989c1dcf38160f82e9b0e72ff070d002d7092
Chaya Deaver <chayadeaver@gmail.com>
2019-07-31 22:51:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,4 +20,4 @@ def using_concat(my_favorite_things, all_my_favs)
   my_favorite_things.concat(all_my_favs)
 end
 def using_insert(list_of_programming_languages, another_language)
-  list_of_programming_languages.insert(another_language = list_of_programming_languages[4])
+  list_of_programming_languages.insert(another_language)
