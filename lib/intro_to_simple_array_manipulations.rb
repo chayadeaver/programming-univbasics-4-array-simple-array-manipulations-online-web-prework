4523c9256056d52eb83d612decaf5a563189e9e4
Chaya Deaver <chayadeaver@gmail.com>
2019-07-31 22:54:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,3 +22,6 @@ end
 def using_insert(list_of_programming_languages, another_language)
   list_of_programming_languages.insert(4, another_language)
 end
+def using_uniq(haircuts)
+  haircuts.uniq
+end
